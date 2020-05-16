131317eb658dc59837619a94dadbb7ecc0d39f64
Mariano Marchetta <mmarchetta@infobytesec.com>
2020-05-16 17:35:54 -0300

    Agrego estilos para login.

--- src/app/modules/authentication/components/Login/Login.js
+++ src/app/modules/authentication/components/Login/Login.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Button, Input } from 'react-native-elements';
+import { Button, Input, Image } from 'react-native-elements';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 import { useAuthentication } from '../../../../shared/hooks/authentication';
 import { transparent } from 'material-ui/styles/colors';
 import styles from '../../../../../assets/stylesheets/login.scss';
+import image from '../../../../../assets/imagenes/login.png';
 
 const Login = ({ navigation, action }) => {
     const [user, setUser] = useState('');
@@ -21,21 +22,28 @@ const Login = ({ navigation, action }) => {
     }, []);
 
     return (
-        <View /*style={styles.barraSuperior}*/>
-            <View style={styles.image}>
-                <Text>IMAGEN</Text>
+        <View style={styles.barraSuperior}>
+            <View>
+                <Image
+                containerStyle={{width:'80%', marginLeft:'auto', marginRight:'auto'}}
+                source={image}
+                style={{ width: '100%', height: 200, marginTop: '40%', resizeMode:'center'}}
+                />
             </View>
-            <View style={styles.body}>
+            <View style={{alignContent: 'center'}}>
                 <Input
+                    containerStyle={{width:'80%', marginLeft:'auto', marginRight:'auto'}}
                     onChangeText={value => setUser(value)}
                     placeholder="Usuario"
                     value={user} />
                 <Input
+                    containerStyle={{width:'80%', marginLeft:'auto', marginRight:'auto'}}
                     onChangeText={value => setPass(value)}
                     placeholder="Contraseña"
+                    secureTextEntry={true}
                     value={pass} />
                 <Button
-                    buttonStyle={{backgroundColor: 'rgb(227, 0, 27)'}}
+                    buttonStyle={{backgroundColor: 'rgb(227, 0, 27)', width:'80%', marginLeft:'auto', marginRight:'auto'}}
                     onPress={() => navigation.navigate("Home")}
                     title={`Iniciar Sesion`} />
                 <Button

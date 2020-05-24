31ff239fa8fb1447d862e680f5b935ecd49fd679
A308293 <magwozdz@santandertecnologia.com.ar>
2020-05-24 19:16:32 -0300

    Agrego navigation/stack

--- src/app/shared/components/aside/Aside.js
+++ src/app/shared/components/aside/Aside.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack';
+
 import ButtonK from '../inputs/ButtonK';
 
 import Home from "../../../modules/home/components/Home";
+import Login from "../../../modules/authentication/components/Login/Login";
 
 const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
 
-const Aside = ({logout}) => (
+const Aside = ({ logout }) => (
     <NavigationContainer>
         <Drawer.Navigator initialRouteName="Home">
             <Drawer.Screen name="Home" component={Home} />
-            <Drawer.Screen name="Logout" component={()=>{ return <ButtonK onPress={logout()}/> }}/>
+            <Drawer.Screen name="Logout" component={() => { return <ButtonK onPress={logout()} /> }} />
         </Drawer.Navigator>
+        <Stack.Navigator>
+            <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
     </NavigationContainer>
 )
 

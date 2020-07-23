d50f168cab0f042b466c61b37708089799baa239
mariano marchetta <mariano.marchetta@hotmail.com>
2020-07-23 19:44:25 -0300

    cambios locales.

--- src/app/shared/services/Account.jsx
+++ src/app/shared/services/Account.jsx
@@ -1,9 +1,11 @@
 import httpClientDecidir from '../../../app/core/config/axios/httpClients/httpClientDecidir'
+import httpClientApi from '../../../app/core/config/axios/httpClients/httpClientApi'
 
 export const login = async () => {
-    return {
-        data: {
-            token: '1231rjno1h41uob3bgv14gv1ug4uo124uo1v4uo14v1o4v1'
-        }
-    }
+    let user = 'usuario_1@valijachica.com.ar';
+    let password = '132456';
+    return httpClientApi.post(`login?email=${user}&password=${password}`);
+    /*data: {
+        token: '1231rjno1h41uob3bgv14gv1ug4uo124uo1v4uo14v1o4v1'
+    }*/
 }
